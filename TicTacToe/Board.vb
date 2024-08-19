dc530589c1e3bba9a11e5feef5182335007e29b5
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-20 02:47:23 +0330

    - Update Draw Board Function - Changing the graphics and the way the game works

--- TicTacToe/Board.vb
+++ TicTacToe/Board.vb
@@ -1,3 +1,16 @@
 ﻿Public Class Board
-
+    Public Sub Draw()
+        Console.WriteLine()
+        Console.ForegroundColor = ConsoleColor.Cyan
+        Console.WriteLine("                ")
+        Console.WriteLine("   ╔═══╦═══╦═══╗")
+        Console.WriteLine("   ║{0}║{1}║{2}║", TicTacToe.Program.XO(0), TicTacToe.Program.XO(1), TicTacToe.Program.XO(2))
+        Console.WriteLine("   ╠═══╬═══╬═══╣")
+        Console.WriteLine("   ║{0}║{1}║{2}║", TicTacToe.Program.XO(3), TicTacToe.Program.XO(4), TicTacToe.Program.XO(5))
+        Console.WriteLine("   ╠═══╬═══╬═══╣")
+        Console.WriteLine("   ║{0}║{1}║{2}║", TicTacToe.Program.XO(6), TicTacToe.Program.XO(7), TicTacToe.Program.XO(8))
+        Console.WriteLine("   ╚═══╩═══╩═══╝")
+        Console.ForegroundColor = ConsoleColor.Gray
+        Console.WriteLine()
+    End Sub
 End Class
--- TicTacToe/Program.vb
+++ TicTacToe/Program.vb
@@ -1,7 +1,11 @@
 ﻿Module Program
 
-    Sub Main()
+    Public XO = {" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 "}
 
+    Sub Main()
+        Dim board As TicTacToe.Board = New TicTacToe.Board()
+        board.Draw()
+        System.Console.ReadKey()
     End Sub
 
 End Module
